76b82655c79f3a41ba24f03f069603751b422d30
Phuong Nguyen <phuong.d.nguyen@gmail.com>
2017-12-08 22:16:11 -0800

    linked list

---
+++ src/net/roostertech/algorithm/linkedlist/AddTwoNumbers.java
@@ -0,0 +1,44 @@
+package net.roostertech.algorithm.linkedlist;
+
+import net.roostertech.algorithm.ListNode;
+
+/**
+ * Created by pnguyen on 12/7/17.
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode a, ListNode b) {
+
+        int carryOver = 0;
+        ListNode result = null, tail = null;
+
+        while (a != null || b != null) {
+            int addition = ((a != null) ? a.val : 0) + ((b != null) ? b.val : 0) + carryOver;
+
+            ListNode newNode = new ListNode(addition % 10);
+            carryOver = addition / 10;
+
+            if (tail != null) {
+                tail.next = newNode;
+                tail = newNode;
+            } else {
+                tail = newNode;
+                result = newNode;
+            }
+
+
+            if (a != null) {
+                a = a.next;
+            }
+            if (b != null) {
+                b = b.next;
+            }
+        }
+
+        if (carryOver > 0) {
+            ListNode newNode = new ListNode(carryOver);
+            tail.next = newNode;
+
+        }
+        return result;
+    }
+}
---
+++ src/net/roostertech/algorithm/linkedlist/CycleDetection.java
@@ -0,0 +1,59 @@
+package net.roostertech.algorithm.linkedlist;
+
+import net.roostertech.algorithm.ListNode;
+import org.junit.Test;
+
+/**
+ * Created by pnguyen on 12/7/17.
+ */
+public class CycleDetection {
+    public ListNode detectCycle(ListNode a) {
+        if (a == null || a.next == null) {
+            return null;
+        }
+        ListNode slow = a.next;
+        ListNode fast = slow == null? null : slow.next;
+
+        boolean hasLoop = false;
+        while (slow != null && fast != null) {
+            if (slow == fast) {
+                hasLoop = true;
+                break;
+            }
+            slow = slow.next;
+            // move 2
+            fast = fast.next;
+            fast = fast == null? null: fast.next;
+        }
+
+//        System.out.println(hasLoop);
+//        System.out.println(cntToLoop);
+
+        if (!hasLoop) {
+            return null;
+        }
+
+        slow = a;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // bad?
+        return slow;
+    }
+
+    @Test
+    public void testDetectCycle() {
+        ListNode head = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        head.next = two;
+        two.next = three;
+        three.next = four;
+        four.next = head;
+
+        System.out.println("loop is at " + detectCycle(head).val);
+    }
+}
