5f5b068ea6cb70c60b11e57ddc40741a8ffdcdf3
Phuong Nguyen <phuong.d.nguyen@gmail.com>
2018-02-07 22:19:20 -0800

    Add another DP version of LIS

--- src/net/roostertech/algorithm/dp/LongestIncreasingSubsequence.java
+++ src/net/roostertech/algorithm/dp/LongestIncreasingSubsequence.java
@@ -1,5 +1,9 @@
 package net.roostertech.algorithm.dp;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
 import java.util.List;
 
 //https://www.interviewbit.com/problems/longest-increasing-subsequence/
@@ -43,5 +47,34 @@ public class LongestIncreasingSubsequence {
 
 //        System.out.println(Arrays.toString(seqLen));
         return maxLen;
+    }
+
+    public int lisv2(final List<Integer> input) {
+        int[] lis = new int[input.size()];
+
+        // Base case, we have LIS of at least 1 from any index
+        Arrays.fill(lis, 1);
+
+        // Calculate the LIS at each index
+        for (int i = 0; i < input.size(); i++) {
+            int maxLis;
+            for (int j = 0; j <= i; j++) {
+                if (input.get(j) < input.get(i) && lis[i] < (lis[j] + 1)) {
+                    lis[i] = lis[j] + 1;
+                }
+            }
+        }
+
+        int maxLis = 0;
+        for (int i = 0; i < input.size(); i++) {
+            maxLis = Math.max(maxLis, lis[i]);
+        }
+
+        return maxLis;
+    }
+    @Test
+    public void testLis() {
+        Assert.assertEquals(6, lis(Arrays.asList(0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15)));
+        Assert.assertEquals(6, lisv2(Arrays.asList(0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15)));
     }
 }
